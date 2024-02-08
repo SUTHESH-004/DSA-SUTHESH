c08ee1956a47b63dec8763427883f1897291dd72
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-02-08 23:19:06 +0530

    Binary Tree implementation

---
+++ BinaryTree.java
@@ -0,0 +1,58 @@
+public class BinaryTree {
+    Node root;
+
+    class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            this.val = val;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    BinaryTree(int val) {
+        root = new Node(val);
+    }
+
+    void insertLeft(Node root, int val) {
+        root.left = new Node(val);
+    }
+
+    void insertRight(Node root, int val) {
+        root.right = new Node(val);
+    }
+    void preorder(Node root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        System.out.print(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+    void inorder(Node root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.val);
+        inorder(root.right);
+    }
+    void postorder(Node root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val);
+    }
+
+}

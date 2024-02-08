f0a315bfc13f9161c41834102113fd8d982ac0a1
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-02-08 23:18:49 +0530

    Linked implementation

---
+++ App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree(0);
+        tree.insertLeft(tree.root, 1);
+        tree.insertRight(tree.root, 2);
+        // System.out.print(tree.root.val);
+        tree.insertLeft(tree.root.left, 3);
+        tree.insertRight(tree.root.left,4);
+        tree.preorder(tree.root);
+        System.out.println();
+        tree.postorder(tree.root);
+        System.out.println();
+        tree.inorder(tree.root);
+
+    }
+}

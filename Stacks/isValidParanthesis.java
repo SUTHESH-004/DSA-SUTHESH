804cde175f4605dc3543a02e4da98ecfc1c78326
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-02-15 22:53:00 +0530

    improved version 2ms to 1ms

--- Stacks/isValidParanthesis.java
+++ Stacks/isValidParanthesis.java
@@ -1,24 +1,29 @@
 package Stacks;
+
 import java.util.*;
-public class isValidParanthesis
- {
+
+class isValidParanthesis {
     public boolean isValid(String s) {
-         Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else {
-                return false; // Invalid character or mismatched brackets
-            }
+        Stack<Character> stack = new Stack<Character>();
+        // String b = "[({";
+        for (char c : s.toCharArray()) {
+            if (c == '{' || c == '(' || c == '[')
+                stack.push(c);
+            else if (!stack.isEmpty()) {
+                if (stack.peek() == '(' && c == ')')
+                    stack.pop();
+                else if (stack.peek() == '{' && c == '}')
+                    stack.pop();
+                else if (stack.peek() == '[' && c == ']')
+                    stack.pop();
+                else
+                    return false;
+            } else
+                return false;
         }
-
-        return stack.isEmpty();
+        if (stack.isEmpty())
+            return true;
+        else
+            return false;
     }
 }
